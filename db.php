b990a5a438a730d615a1e3df07ba66c3758371e5
Onurcan Genç <onurcan.genc@ug.bilkent.edu.tr>
2024-05-19 17:41:27 +0300

    Cloud Integration removed. All docker files purged.

--- db.php
+++ db.php
@@ -1,22 +1,16 @@
 <?php
 
-$host = getenv('DB_HOST') ?: 'mysql';
+$host = getenv('DB_HOST') ?: 'sql112.infinityfree.com';
 $port = getenv('DB_PORT') ?: '3306';
-$dbname = getenv('DB_DATABASE') ?: 'test';
-$user = getenv('DB_USERNAME') ?: 'root';
-$pass = getenv('DB_PASSWORD') ?: 'root';
-
-echo "Host: $host<br>";
-echo "Port: $port<br>";
-echo "Database: $dbname<br>";
-echo "User: $user<br>";
+$dbname = getenv('DB_DATABASE') ?: 'if0_36576984_test';
+$user = getenv('DB_USERNAME') ?: 'if0_36576984';
+$pass = getenv('DB_PASSWORD') ?: 'Alvin1402';
 
 $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
 
 try {
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "DB Connection Successful";
 } catch (PDOException $ex) {
     echo "DB Connection Error: " . $ex->getMessage();
 }
