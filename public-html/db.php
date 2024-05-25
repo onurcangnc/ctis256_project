16d5d5ef1932715431a9d43dfa323c125d52103f
Onurcan Genç <onurcan.genc@ug.bilkent.edu.tr>
2024-05-25 20:06:09 +0300

    Update1

--- public-html/db.php
+++ public-html/db.php
@@ -1,15 +1,19 @@
 <?php
-
-$dsn = "mysql:host=localhost;port=3306;dbname=ctisproj_1;charset=utf8mb4";
+$dsn = "mysql:host=mysql;port=3306;dbname=ctisproj;charset=utf8mb4";
 $user = "ctisproj";
 $pass = "b20Pp7U0xv";
-$dbname = 'ctisproj_1';
+$dbname = 'ctisproj';
 
 try {
+    // Create a new PDO instance
     $pdo = new PDO($dsn, $user, $pass);
+    // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
+    // Confirmation message
+    print("Connected to the $dbname database successfully.");
 } catch (PDOException $ex) {
+    // Handle connection error
     echo "DB Connection Error: " . $ex->getMessage();
+    print("Connection failed: " . $ex->getMessage());
 }
+?>
